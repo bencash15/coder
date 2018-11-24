4a213154592b11489356e44f07787ea65cad2751
bencash15 <45306075+bencash15@users.noreply.github.com>
2018-11-24 16:09:01 -0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
 
 firebase.initializeApp({
-  messagingSenderId: '454684930251'
+  messagingSenderId: '446303109584'
 });
 
 const messaging = firebase.messaging();
